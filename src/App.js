27b55092b1b952b355175b8316a8b568be8b2370
Jatinchourasia <jatinchourasia3@gmail.com>
2022-03-08 11:13:19 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Canvas } from "@react-three/fiber";
+import React, { Suspense } from "react";
+import styled from "styled-components";
+import { Sphare } from "./components/canvas/Sphare";
 
-function App() {
+const CanvasContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CanvasContainer>
+      <Canvas>
+        <Suspense fallback={null}>
+          <Sphare />
+        </Suspense>
+      </Canvas>
+    </CanvasContainer>
   );
-}
-
-export default App;
+};
+//
---
+++ src/components/canvas/Sphare.jsx
@@ -0,0 +1,41 @@
+import { useFrame, useLoader } from "@react-three/fiber";
+import React, { useRef } from "react";
+import { TextureLoader } from "three";
+import NormapMap from "../../static/texture/NormalMap.png";
+import Golf from "../../static/texture/golf.jpg";
+import { OrbitControls } from "@react-three/drei";
+export const Sphare = (props) => {
+  const [normalMap, golfMap] = useLoader(TextureLoader, [NormapMap, Golf]);
+
+  const sphareRef = useRef(null);
+  useFrame(({ clock }) => {
+    const elapsedTime = clock.getElapsedTime();
+    sphareRef.current.rotation.y = elapsedTime / 2;
+  });
+  return (
+    <>
+      <pointLight position={[1, -1.9, -1.6]} intensity={10} color="#3a1b8d" />
+      <pointLight position={[-1, 0.5, -1.6]} intensity={10} color="#640707" />
+      <ambientLight intensity={2} />
+      <mesh ref={sphareRef}>
+        <sphereGeometry args={[1, 64, 64]} />
+        {/* <meshPhongMaterial color="red" /> */}
+        <meshStandardMaterial
+          metalness={0.7}
+          roughness={0.2}
+          map={golfMap}
+          normalMap={normalMap}
+        />
+
+        <OrbitControls
+          enableZoom={true}
+          enablePan={true}
+          enableRotate={true}
+          zoomSpeed={0.6}
+          panSpeed={0.5}
+          rotateSpeed={0.5}
+        />
+      </mesh>
+    </>
+  );
+};
